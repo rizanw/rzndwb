74e78904586a2ad1f6761828d8cdd35a03be5e90
rizanw <rzkandre@gmail.com>
2019-12-11 20:40:34 +0700

    init

---
+++ src/app/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/header";
+import { Home } from "./components/home";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-10 col-xs-offset-1">
+                            <Home />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App />, window.document.getElementById("app"));
